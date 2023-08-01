702badbfcb3cb2e198019f4bd877b1bdecd81875
anshul46 <89180266+AnshB0712@users.noreply.github.com>
2023-08-01 18:41:01 +0530

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,14 @@ import ProductLists from './components/ProductLists'
 
 function App() {
   return (
-    <div className="App">
-     <ProductLists/>
-    </div>
+    <>
+     <p style={{textAlign:'center',fontSize:'2rem'}}>
+      <a href='https://github.com/AnshB0712/Parekh_Assign'>Source Code</a>
+     </p>
+     <div className="App">
+      <ProductLists/>
+     </div>
+    </>
   )
 }
 
